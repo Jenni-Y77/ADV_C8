aaa268965313090146f668730dd9316c11117cf8
Jenni-Y77 <87382789+Jenni-Y77@users.noreply.github.com>
2021-10-07 12:18:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ function gotResults(error, results) {
     random_number_g = Math.floor(Math.random() * 255) + 1;
     random_number_b = Math.floor(Math.random() * 255) + 1;
 
-    document.getElementById("result_label").innerHTML = 'I can hear - '+ results[0].label;
-    document.getElementById("result_confidence").innerHTML = 'Accuracy - '+ (results[0].confidence*100).toFixed(2)+" %";
+    document.getElementById("result_label").innerHTML = 'Puedo escuchar - '+ results[0].label;
+    document.getElementById("result_confidence").innerHTML = 'Precisión - '+ (results[0].confidence*100).toFixed(2)+" %";
     document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
     document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
 
@@ -27,7 +27,7 @@ function gotResults(error, results) {
     img2 = document.getElementById('alien3')
     img3 = document.getElementById('alien4')
 
-    if (results[0].label == "Clap") {
+    if (results[0].label == "Aplauso") {
       img.src = 'aliens-01.gif';
       img1.src = 'aliens-02.png';
       img2.src = 'aliens-03.png';
